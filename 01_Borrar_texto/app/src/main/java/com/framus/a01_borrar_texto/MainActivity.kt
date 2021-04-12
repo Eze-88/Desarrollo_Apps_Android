c624785d5ec895a47c7ec85c37ec406e7dd0f8fc
Eze-88 <ezequielap@gmail.com>
2021-04-12 20:11:21 -0300

    Comentarios agregados

--- 01_Borrar_texto/app/src/main/java/com/framus/a01_borrar_texto/MainActivity.kt
+++ 01_Borrar_texto/app/src/main/java/com/framus/a01_borrar_texto/MainActivity.kt
@@ -16,9 +16,11 @@ class MainActivity : AppCompatActivity() {
         //Se define el mensaje mostrado
         cartel.text = "Borrame"
 
+        //Establezco el valor inicial del tamaño del texto, para tenerlo conocido
         var tamanio = cartel.textSize
         cartel.setTextSize(TypedValue.COMPLEX_UNIT_SP, tamanio)
 
+        //CAMBIO EL TEXTO
         //Definición de la variable boton_borrado y la asociación al boton
         val boton_borrado: Button = findViewById(R.id.boton_borrado)
         //Se define la acción al presionar el boton
@@ -26,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             cartel.text = ":|"
         }
 
+        //ACHICO EL ETXTO
         //Definición de la variable boton_menos y la asociación al boton
         val boton_menos: Button = findViewById(R.id.boton_menos)
         //Se define la acción al presionar el boton
@@ -34,6 +37,7 @@ class MainActivity : AppCompatActivity() {
             cartel.setTextSize(TypedValue.COMPLEX_UNIT_SP, tamanio)
         }
 
+        //AGRANDO EL TEXTO
         //Definición de la variable boton_mas y la asociación al boton
         val boton_mas: Button = findViewById(R.id.boton_mas)
         //Se define la acción al presionar el boton
