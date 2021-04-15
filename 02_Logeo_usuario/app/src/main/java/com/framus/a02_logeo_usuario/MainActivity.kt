cd1ee2c73475915e4bfa7b226d15036ea59d64d9
Eze-88 <ezequielap@gmail.com>
2021-04-15 15:04:39 -0300

    Ingreso usuario

--- 02_Logeo_usuario/app/src/main/java/com/framus/a02_logeo_usuario/MainActivity.kt
+++ 02_Logeo_usuario/app/src/main/java/com/framus/a02_logeo_usuario/MainActivity.kt
@@ -22,22 +22,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //+++++DEFINICIONES+++++
         //Definición de la variable para asociar al monitor
         val cartel: TextView = findViewById(R.id.Monitor)
-        //Se deja en blanco
         cartel.text = ""
-
-        //Defino el snackbar para el ususario
-        val root_layout: ConstraintLayout = findViewById(R.id.root_layout)
-
-        //Ingreso del usuario
-        val casilla_usuario: EditText = findViewById(R.id.casilla_usuario)
-
-        //Pruebo el ingreso del texto
+        //Definición del botón para ingresar
         val login: Button = findViewById(R.id.login)
+        login.text = "Ingresar"
+        //Defino el snackbar para el ususario y la contraseña
+        val root_layout: ConstraintLayout = findViewById(R.id.root_layout)
+        //Casilla de ingreso de usuario
+        val casilla_usuario: EditText = findViewById(R.id.casilla_usuario)
+        //Casilla de ingreso de la contraseña
+        val casilla_contra: EditText = findViewById(R.id.casilla_contra)
+
+        //+++++ACCIONES+++++
+        //Comportamiento del botón
         login.setOnClickListener {
-            if (casilla_usuario.length()>0)
-                cartel.text = casilla_usuario.text
+            if (casilla_usuario.length()>0){
+                if (casilla_contra.length()>0)
+                    cartel.text = "Datos ingresados OK"
+                else
+                    Snackbar.make(root_layout,"Contraseña en blanco", Snackbar.LENGTH_SHORT).show()
+            }
             else
                 Snackbar.make(root_layout,"Ingrese el usuario", Snackbar.LENGTH_SHORT).show()
         }
