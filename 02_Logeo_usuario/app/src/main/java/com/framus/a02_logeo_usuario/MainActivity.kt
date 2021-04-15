bba57ff3d74c75ca1a8dd52f4db022f9fe2f09a3
Eze-88 <ezequielap@gmail.com>
2021-04-15 12:49:01 -0300

    Ingreso usuario

--- 02_Logeo_usuario/app/src/main/java/com/framus/a02_logeo_usuario/MainActivity.kt
+++ 02_Logeo_usuario/app/src/main/java/com/framus/a02_logeo_usuario/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.framus.a02_logeo_usuario
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.framus.Entidades.Persona
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,12 +27,19 @@ class MainActivity : AppCompatActivity() {
         //Se deja en blanco
         cartel.text = ""
 
+        //Defino el snackbar para el ususario
+        val root_layout: ConstraintLayout = findViewById(R.id.root_layout)
 
+        //Ingreso del usuario
+        val casilla_usuario: EditText = findViewById(R.id.casilla_usuario)
 
-        //Cargo hardcoded la variable creada para probar
-        miUser = Persona ("Eze", "1935")
-
-        //Imprimo en el LOG la variable creada
-        Log.d("PRUEBA",miUser.usuario+" "+miUser.contrasenia)
+        //Pruebo el ingreso del texto
+        val login: Button = findViewById(R.id.login)
+        login.setOnClickListener {
+            if (casilla_usuario.length()>0)
+                cartel.text = casilla_usuario.text
+            else
+                Snackbar.make(root_layout,"Ingrese el usuario", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
