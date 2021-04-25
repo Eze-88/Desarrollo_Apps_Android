9c993f3504bc95aa9613fb5cac5facbe5f15e7fe
Eze-88 <ezequielap@gmail.com>
2021-04-25 14:17:23 -0300

    03 Creo variables

--- 03_Fragmentos/app/src/main/java/com/framus/Fragmentos/Pant_logeo.kt
+++ 03_Fragmentos/app/src/main/java/com/framus/Fragmentos/Pant_logeo.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.findNavController
+import com.framus.Entidades.Persona
 import com.framus.a03_fragmentos.R
 import kotlinx.android.synthetic.main.fragment_pant_logeo.*
 import kotlinx.android.synthetic.main.fragment_pant_logeo.view.*
@@ -19,8 +20,15 @@ import kotlinx.android.synthetic.main.fragment_pant_logeo.view.*
  */
 class Pant_logeo : Fragment() {
 
+    //+++++DEFINICIONES GLOBALES+++++
+    //Definicion de la variable para referenciar la vista
     lateinit var v: View
+    //Creo el boton de logeo
     lateinit var btn_logeo: Button
+    //Creo el monitor
+    lateinit var monitor: TextView
+    //Creo la lista de usuarios
+    var Personas : MutableList<Persona> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,16 +37,19 @@ class Pant_logeo : Fragment() {
         // Inflate the layout for this fragment
         v =  inflater.inflate(R.layout.fragment_pant_logeo, container, false)
 
+        //+++++ASOCIACIONES+++++
+        //Boton de ingreso
         btn_logeo = v.findViewById(R.id.login)
+        btn_logeo.text = "Ingresar"
+        //Monitor
+        monitor = v.findViewById(R.id.cartel)
+        monitor.text = ""
 
         return v
     }
 
     override fun onStart() {
         super.onStart()
-
-        val monitor: TextView = v.findViewById(R.id.cartel)
-        monitor.text = ""
 
         btn_logeo.setOnClickListener {
             val action = Pant_logeoDirections.actionPantLogeoToPantPrinc()
