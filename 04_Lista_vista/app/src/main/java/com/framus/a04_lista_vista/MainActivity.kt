3fa7393d5d54839cd70c617043c097ebef7ab743
Eze-88 <ezequielap@gmail.com>
2021-04-26 09:09:37 -0300

    04 Creado

---
+++ 04_Lista_vista/app/src/main/java/com/framus/a04_lista_vista/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.framus.a04_lista_vista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
