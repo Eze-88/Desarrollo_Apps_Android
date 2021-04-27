d46b5fae3437e4a93e6409e68561ad4c36574c1d
Eze-88 <ezequielap@gmail.com>
2021-04-27 08:37:35 -0300

    arranco 04

--- 04_Lista_vista/app/src/main/java/com/framus/Fragmentos/Pant_logeo.kt
+++ 04_Lista_vista/app/src/main/java/com/framus/Fragmentos/Pant_logeo.kt
@@ -16,11 +16,6 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_pant_logeo.*
 import kotlinx.android.synthetic.main.fragment_pant_logeo.view.*
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Pant_logeo.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Pant_logeo : Fragment() {
 
     //+++++DEFINICIONES GLOBALES+++++
@@ -91,7 +86,7 @@ class Pant_logeo : Fragment() {
                             encontrado = true
                         if (encontrado) {
                             if (Personas[cont].contrasenia.equals(casilla_contra.text.toString())) {
-                                val action = Pant_logeoDirections.actionPantLogeoToPantPrinc(Personas[cont].contrasenia)
+                                val action = Pant_logeoDirections.actionPantLogeoToPantPrinc(Personas[cont].usuario)
                                 v.findNavController().navigate(action)
                                 break
                             } else {
--- 04_Lista_vista/app/src/main/java/com/framus/Fragmentos/Pant_princ.kt
+++ 04_Lista_vista/app/src/main/java/com/framus/Fragmentos/Pant_princ.kt
@@ -8,16 +8,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.framus.a04_lista_vista.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Pant_princ.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Pant_princ : Fragment() {
 
     //Definicion de la variable para referenciar la vista
@@ -36,28 +26,12 @@ class Pant_princ : Fragment() {
         monitor = v.findViewById(R.id.cartel2)
 
         var usuario  = Pant_princArgs.fromBundle(requireArguments()).Usuario
-        monitor.text = usuario
+        monitor.text = "El usuario ingresado es " + usuario
 
         return v
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Pant_princ.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Pant_princ().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
     }
 }
