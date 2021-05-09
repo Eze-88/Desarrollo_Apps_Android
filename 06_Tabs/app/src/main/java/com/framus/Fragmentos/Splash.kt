52ece8a21123ddff2e1db430fa26e827ffc23de3
Eze Arigon <ezequielap@gmail.com>
2021-05-09 14:43:19 -0300

    06 comienzo splash

---
+++ 06_Tabs/app/src/main/java/com/framus/Fragmentos/Splash.kt
@@ -0,0 +1,24 @@
+package com.framus.Fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.framus.a06_tabs.R
+
+class Splash : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+}
