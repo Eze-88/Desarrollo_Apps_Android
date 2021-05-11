0c0f602d89baa20f70799ddf0cefdae8f5d80994
Eze Arigon <ezequielap@gmail.com>
2021-05-11 17:01:45 -0300

    06 splash listo

--- 06_Tabs/app/src/main/java/com/framus/a06_tabs/Splash.kt
+++ 06_Tabs/app/src/main/java/com/framus/a06_tabs/Splash.kt
@@ -1,10 +1,14 @@
 package com.framus.a06_tabs
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +19,11 @@ class Splash : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 2000)
+
+        Glide
+            .with(applicationContext)
+            .load("https://dbdzm869oupei.cloudfront.net/img/vinylrugs/preview/18784.png")
+            .centerInside()
+            .into(findViewById(R.id.imageView))
     }
 }
