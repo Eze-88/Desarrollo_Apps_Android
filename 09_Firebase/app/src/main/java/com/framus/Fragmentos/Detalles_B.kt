21e7ef741d9d9be8b1e3f2a43d3183fd33b888a1
Eze Arigon <ezequielap@gmail.com>
2021-07-03 15:49:29 -0300

    09 muestro detalles a y b

--- 09_Firebase/app/src/main/java/com/framus/Fragmentos/Detalles_B.kt
+++ 09_Firebase/app/src/main/java/com/framus/Fragmentos/Detalles_B.kt
@@ -2,6 +2,7 @@ package com.framus.Fragmentos
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +15,8 @@ import com.framus.BaseDeDatos.appDatabase
 import com.framus.BaseDeDatos.discosDAO
 import com.framus.Entidades.Discos
 import com.framus.a09_firebase.R
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 
 class Detalles_B : Fragment() {
 
@@ -25,9 +28,11 @@ class Detalles_B : Fragment() {
     private var db: appDatabase? = null
     private var discosDAO: discosDAO? = null
     //Variable auxiliar para encontrar el disco a mostrar
-    var cd:  MutableList<Discos> = mutableListOf()
+    //var cd:  MutableList<Discos> = mutableListOf()
     //Variable auxiliar para identificar el disco a mostrar
     var pos: Int = 0
+    //Base de datos online
+    private val bd = FirebaseFirestore.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,23 +48,37 @@ class Detalles_B : Fragment() {
         db = appDatabase.getAppDataBase(v.context)
         discosDAO = db?.discosDAO()
 
-        //Identifico el disco a mostrar
-        cd = discosDAO?.loadAllPersons() as MutableList<Discos>
-        val id: Int = Detalles_A.identificador
-        for ( i in 0 until cd.size){
-            if (cd[i].id == id){
-                pos = i
-                break
+//        //Identifico el disco a mostrar
+//        cd = discosDAO?.loadAllPersons() as MutableList<Discos>
+//
+//        val id: Int = Detalles_A.identificador
+//
+//        for ( i in 0 until cd.size){
+//            if (cd[i].id == id){
+//                pos = i
+//                break
+//            }
+//        }
+
+        bd.collection("albums").document(Detalles_A.identificador.toString()).get().addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot != null){
+                val cd = dataSnapshot.toObject<Discos>()
+                if (cd != null) {
+                    Log.d("PERRO","Exito")
+                    //Muestro la caratula
+                    Glide
+                        .with(requireContext())
+                        .load(cd.caratula)
+                        .centerInside()
+                        .into(getImageView())
+                    getImageView()
+                }
+            } else {
+                Log.d("PERRO", "No existe el documento")
             }
         }
 
-        //Muestro la caratula
-        Glide
-            .with(requireContext())
-            .load(cd[pos].caratula)
-            .centerInside()
-            .into(getImageView())
-        getImageView()
+
 
         //Preferencias
         val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
