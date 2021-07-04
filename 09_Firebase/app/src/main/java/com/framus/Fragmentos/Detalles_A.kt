a286506e6846e4aba11dd5895d7bc97a2b9979b8
Eze Arigon <ezequielap@gmail.com>
2021-07-04 14:42:21 -0300

    09 ubicacion funcionando

--- 09_Firebase/app/src/main/java/com/framus/Fragmentos/Detalles_A.kt
+++ 09_Firebase/app/src/main/java/com/framus/Fragmentos/Detalles_A.kt
@@ -1,8 +1,15 @@
 package com.framus.Fragmentos
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,13 +17,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import androidx.navigation.findNavController
 import androidx.preference.PreferenceManager
 import com.framus.BaseDeDatos.appDatabase
 import com.framus.BaseDeDatos.discosDAO
 import com.framus.Entidades.Discos
 import com.framus.a09_firebase.R
+import com.google.android.gms.location.*
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
 import kotlinx.coroutines.CoroutineScope
@@ -50,7 +62,8 @@ class Detalles_A : Fragment() {
     //Base de datos online
     private val bd = FirebaseFirestore.getInstance()
 
-    var prueba: String = "Hola"
+    val PERMISSION_ID = 42
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -83,8 +96,8 @@ class Detalles_A : Fragment() {
         val scope = CoroutineScope(Dispatchers.Default + parentJob)
 
         scope.launch {
-            configuro_ubicacion()
             obtengo_ubicacion()
+            calculo_distancia()
         }
 
         bd.collection("albums").document(identificador.toString()).get().addOnSuccessListener { dataSnapshot ->
@@ -140,14 +153,101 @@ class Detalles_A : Fragment() {
         }
     }
 
-    suspend fun configuro_ubicacion(){
-        prueba = "Como estas"
-        Log.d("UBIC","GPS configurado"+prueba)
+    suspend fun obtengo_ubicacion(){
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        getLastLocation()
+
+        Log.d("UBIC","GPS configurado")
     }
 
-    suspend fun obtengo_ubicacion(){
-        prueba = "Normal"
-        Log.d("UBIC","Ubicacion obeniteda"+prueba)
+    suspend fun calculo_distancia(){
+        Log.d("UBIC","Ubicacion obeniteda")
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(requireActivity()) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        Log.d ("UBICACION",location.latitude.toString())
+                        Log.d ("UBICACION",location.longitude.toString())
+                    }
+                }
+            } else {
+                Toast.makeText(requireContext(), "Turn on location", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            Log.d ("Test",mLastLocation.latitude.toString())
+            Log.d ("Test",mLastLocation.longitude.toString())
+//            findViewById<TextView>(R.id.latTextView).text = mLastLocation.latitude.toString()
+//            findViewById<TextView>(R.id.lonTextView).text = mLastLocation.longitude.toString()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_ID
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLastLocation()
+            }
+        }
     }
 
     companion object{
